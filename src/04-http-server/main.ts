aa81f8b59e1c175852fd4b8ac872306f7acd22b0
Keerati Limkulphong <imKrish@Keeratis-MacBook-Pro.local>
2016-10-13 10:55:20 +1300

    Clean the code

--- src/04-http-server/main.ts
+++ src/04-http-server/main.ts
@@ -6,26 +6,32 @@ import * as mime from 'mime'
 const log = console.log 
 
 http.createServer((req, res) => {
-    log(req.url)
+
     const dir = '/'
     let uri = url.parse(<string>req.url).pathname
     if (uri == '/') {
         uri = 'index.html'
     }
     const filename = path.join(dir, uri)
+
+    // Debug
+    log(req.url)
     log(path.join(dir, uri))
-    log(__dirname + filename)
+    log(__dirname + filename) // Absolute path
+
     fs.readFile(__dirname + filename, (err, data) => {
             if (err) {
                 res.writeHead(500)
                 return res.end('Error loading index.html')
             }
-            log(data)
-            log(filename + ' has read')
             res.setHeader('content-type', mime.lookup(filename))
-            log(mime.lookup(filename))
             res.writeHead(200)
             res.end(data)
+
+            // Debug
+            log(data) // Buffer
+            log(filename + ' has read')
+            log(mime.lookup(filename)) // ex. application/javascript
         })
 
 }).listen(8888)
