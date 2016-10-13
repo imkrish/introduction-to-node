d888530df2dad617218d53855f5ab78c9954eb48
Keerati Limkulphong <imKrish@Keeratis-MacBook-Pro.local>
2016-10-13 21:42:13 +1300

    Use of fs - example of christmas tree

---
+++ src/08-file-system/fs-async.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs'
+
+if (fs.existsSync('temp')) {
+    console.log('Directory exists, removing...')
+    if (fs.existsSync('temp/new.txt')) {
+        fs.unlinkSync('temp/new.txt')
+    }
+    fs.rmdirSync('temp')
+}
+
+fs.mkdir('temp', (_) => {
+    fs.exists('temp', (exists) => {
+        if (exists) {
+            process.chdir('temp')
+            fs.writeFile('test.txt', 'This is some test text for the file', (_) => {
+                fs.rename('test.txt', 'new.txt', (_) => {
+                    fs.stat('new.txt', (_, stats) => {
+                        console.log('File has size: ', stats.size + ' bytes')
+                        fs.readFile('new.txt', (_, data) => {
+                            console.log('File contents: ', data.toString())
+                        })
+                    })
+                })
+            })
+        }
+    })
+})
