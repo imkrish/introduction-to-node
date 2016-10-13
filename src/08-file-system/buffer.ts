78f83d5b9e88e0ab419271d193535e973f168805
Keerati Limkulphong <imKrish@Keeratis-MacBook-Pro.local>
2016-10-13 21:49:51 +1300

    Buffer example

---
+++ src/08-file-system/buffer.ts
@@ -0,0 +1,13 @@
+const b = new Buffer('Hello')
+
+console.log(b)
+
+console.log(b.toString())
+
+console.log(b.toString('base64'))
+
+const v = new Buffer('World').toString('base64')
+
+console.log(v)
+
+console.log(b.toString('utf-8', 0, 2))
